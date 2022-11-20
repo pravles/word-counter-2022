b82cfb2857e64054b7e7acf98ac0f8df4a5c24c1
Pravles Redneckoff <pravles@pm.me>
2022-11-20 14:38:22 +0300

    I-1: Implementing extracting data from the command-line arguments.

--- src/main/java/com/pravles/wordcounter/App.java
+++ src/main/java/com/pravles/wordcounter/App.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 
 import static com.pravles.wordcounter.Utils.calculateCenterOnScreen;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.defaultString;
 
 public class App {
     private final static Logger logger = LoggerFactory.getLogger(App.class);
@@ -17,12 +19,31 @@ public class App {
             window.setVisible(true);
             window.setLocation(calculateCenterOnScreen(window));
         } else {
-            logger.error("No configuration file specified");
+            logger.error(format("No configuration file specified or "+
+                            "configuration is invalid ('%s')",
+                    defaultString(argsParsingResult.message(), "")));
         }
     }
 
     Outcome<File> extractConfigFile(final String[] args) {
-        return new Outcome<File>().success(false);
+        if (args == null) {
+            return new Outcome<File>().success(false).message("args is null");
+        }
+        if (args.length != 1) {
+            return new Outcome<File>().success(false)
+                    .message(format("Incorrect number of arguments, " +
+                            "got %d instead of 1", args.length));
+        }
+        final String configFilePath = args[0];
+        final File configFile = new File(configFilePath);
+        if (!configFile.exists() || !configFile.isFile() ||
+                !configFile.canRead()) {
+            return new Outcome<File>().success(false)
+                    .message(format("Configuration file '%s' either does " +
+                            "not exist, is not a file or not readable",
+                            configFile.getAbsolutePath()));
+        }
+        return new Outcome<File>().success(true).value(configFile);
     }
 
     public static void main(final String[] args) {
--- src/main/java/com/pravles/wordcounter/Outcome.java
+++ src/main/java/com/pravles/wordcounter/Outcome.java
@@ -8,4 +8,5 @@ import lombok.experimental.Accessors;
 public class Outcome<C> {
     private boolean success;
     private C value;
+    private String message;
 }
