63fb050be771e190c2a6bb2ec07456f30318273d
Pravles Redneckoff <pravles@pm.me>
2022-11-20 14:21:50 +0300

    I-1: Logging seems to work.

--- src/main/java/com/pravles/wordcounter/App.java
+++ src/main/java/com/pravles/wordcounter/App.java
@@ -1,9 +1,17 @@
 package com.pravles.wordcounter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.pravles.wordcounter.Utils.calculateCenterOnScreen;
+
 public class App {
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(final String[] args) {
+        logger.info("Hello");
         final MainWindow window = new MainWindow();
         window.setVisible(true);
-        window.setLocation(Utils.calculateCenterOnScreen(window));
+        window.setLocation(calculateCenterOnScreen(window));
     }
 }
