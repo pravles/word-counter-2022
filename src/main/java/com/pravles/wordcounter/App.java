b528ffa39208c0c600fe4be3727b9cbe8d9ef154
Pravles Redneckoff <pravles@pm.me>
2022-11-20 14:05:33 +0300

    I-1: Now we center the window on the screen.

--- src/main/java/com/pravles/wordcounter/App.java
+++ src/main/java/com/pravles/wordcounter/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(final String[] args) {
         final MainWindow window = new MainWindow();
         window.setVisible(true);
+        window.setLocation(Utils.calculateCenterOnScreen(window));
     }
 }
